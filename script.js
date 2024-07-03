3ba2db2cc1bf3d4847d7b2ce90486faaa472084c
10-menachi <bobitlmr188221@spu.ac.ke>
2024-07-03 16:21:17 +0300

    Add Content Section

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-alert("Hello World");
+const time = document.querySelector(".time-span");
+const day = document.querySelector(".day-span");
+time.textContent = new Date().toLocaleTimeString("en-GB", {
+  hour: "2-digit",
+  minute: "2-digit",
+  second: "2-digit",
+  timeZone: "UTC",
+});
+day.textContent = new Date().toLocaleDateString("en-GB", {
+  weekday: "long",
+});
